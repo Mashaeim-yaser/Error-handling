5ac98a24cfcfad590de9f1226eff158e0b0fdf14
mashaeim-yaser <mashaeimyaser@gmail.com>
2022-08-30 21:25:07 +0500

    initial commit

---
+++ script1.js
@@ -0,0 +1,15 @@
+x = 4
+y = 6
+function myFunction(x,y) {
+  equation = x^2+5*y ;
+  }
+
+  myFunction(4,6);
+  {
+    try {
+      x**2(0);   
+    }
+    catch(err) {
+        console.log(x**2+6*5);
+    }
+    }
